cdbb066c37eb4b35bb19fb403e85e9a3f956bf1e
Qiwenweiii <Qiwenweiii@foxmail.com>
2022-09-29 16:17:28 +0800

    初始化项目，完成左侧菜单栏的tab部分

---
+++ components/Sidebar/Left/Tab.vue
@@ -0,0 +1,25 @@
+<template>
+  <div
+    class="flex items-center p-3 w-min rounded-full hover:bg-gray-200 dark:hover:bg-dim-200 dark:text-white transition duration-300"
+  >
+    <div class="w-6 h-6 text-dark">
+      <slot name="icon" />
+    </div>
+    <div class="ml-2 text-xl hidden xl:block" :class="textClasses">
+      <slot name="name" />
+    </div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  active: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const textClasses = computed(() =>
+  props.active ? 'font-semibold' : 'font-normal'
+);
+</script>
